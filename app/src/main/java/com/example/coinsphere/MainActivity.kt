dbff7c164d2ea53fad66b8f2fa49e8c3ca10245a
MiguelMH04 <mmh79381@lasallebajio.edu.mx>
2025-09-07 01:06:12 -0600

    Creacion de la LazyColumn

--- app/src/main/java/com/example/coinsphere/MainActivity.kt
+++ app/src/main/java/com/example/coinsphere/MainActivity.kt
@@ -8,11 +8,17 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
@@ -26,6 +32,8 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil3.compose.AsyncImage
+import com.example.coinsphere.models.coins
 import com.example.coinsphere.ui.theme.Background
 import com.example.coinsphere.ui.theme.CoinSphereTheme
 import com.example.coinsphere.ui.theme.Surface
@@ -39,7 +47,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CoinSphereTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-
+                HomeScreen(innerPadding)
                 }
             }
         }
@@ -47,7 +55,7 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun HomeScreen(){
+fun HomeScreen(innerPadding: PaddingValues){
     Column (
         modifier = Modifier
             .fillMaxSize()
@@ -167,6 +175,51 @@ fun HomeScreen(){
                 .height(3.dp)
                 .background(Surface)
         )
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(10.dp)
+        ) {
+            items(coins){ CoinList -> 
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clip(shape = RoundedCornerShape(10.dp))
+                    .background(Surface)
+                    .padding(horizontal = 10.dp, vertical = 10.dp)
+            ) {
+                   Text(CoinList.number.toString(),
+                       color = TextDim,
+                       modifier = Modifier
+                           .weight(.5f
+                               ))
+
+                   AsyncImage(model = CoinList.image,
+                       contentDescription = CoinList.title,
+                       modifier = Modifier
+                           .size(35.dp)
+                           .weight(.8f, fill = false))
+                Spacer(modifier = Modifier
+                    .width(8.dp))
+
+                    Text(CoinList.title,
+                        color = TextDim,
+                        modifier = Modifier
+                            .weight(2f
+                            ))
+
+                    Text(CoinList.price,
+                        color = TextDim,
+                        modifier = Modifier
+                            .weight(2f
+                            ))
+
+
+               }
+                Spacer(modifier = Modifier
+                    .padding(top = 10.dp))
+            }
+        }
     }
 }
 @Preview
@@ -178,6 +231,6 @@ showSystemUi = true
 @Composable
 fun GreetingPreview() {
     CoinSphereTheme {
-        HomeScreen()
+        HomeScreen(innerPadding = PaddingValues(all = 10.dp))
     }
 }
